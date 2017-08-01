7cacbd9f2cf19ac4c5cfd438009bf8a6bd902f81
Thanawat Hirunyanitiwattana <oat_war_time@hotmail.com>
2017-08-01 16:31:55 +0700

    Add foundation local var but not working

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,11 @@ module.exports = function(config) {
     browsers: ['Chrome'],
     singleRun: true,
     frameworks: ['mocha'],
-    files: ['app/test/**/*.test.jsx'],
+    files: [
+      'node_modules/jquery/dist/jquery.min.js',
+      'node_modules/foundation-sites/dist/foundation.min.js',
+      'app/test/**/*.test.jsx'
+    ],
     preprocessors: {
       'app/test/**/*.test.jsx': ['webpack','sourcemap']
     },
