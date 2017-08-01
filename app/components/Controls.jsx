9217d04983ff920f5d38948430f41acc906d777a
Thanawat Hirunyanitiwattana <oat_war_time@hotmail.com>
2017-08-01 11:01:48 +0700

    Add static controls panel component

---
+++ app/components/Controls.jsx
@@ -0,0 +1,26 @@
+let React = require('react');
+
+let Controls = React.createClass({
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired
+  },
+  render: function() {
+    let {countdownStatus} = this.props;
+    let renderStartStopButton = () =>{
+      if(countdownStatus === 'started'){
+        return <button className="button secondary">Pause</button>
+      }else if(countdownStatus === 'paused'){
+        return <button className="button primary">Start</button>
+      }
+    };
+    
+    return (
+      <div className="controls">
+        {renderStartStopButton()}
+        <button className="button alert hollow">Clear</button>
+      </div>
+    );
+  }
+});
+
+module.exports = Controls;
---
+++ app/test/components/Controls.test.jsx
@@ -0,0 +1,31 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+let expect = require('expect');
+let $ = require('jQuery');
+let TestUtils = require('react-addons-test-utils');
+
+let Controls = require('Controls');
+
+describe('Controls', () => {
+  it('should exist', () => {
+    expect(Controls).toExist();
+  });
+
+  describe('render', () => {
+    it('should render pause when started', () => {
+      let controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started" />);
+      let $el = $(ReactDOM.findDOMNode(controls));
+      let $pauseButton = $el.find('button:contains(Pause)');
+
+      expect($pauseButton.length).toBe(1);
+    });
+
+    it('should render start when paused', () => {
+      let controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused" />);
+      let $el = $(ReactDOM.findDOMNode(controls));
+      let $startButton = $el.find('button:contains(Start)');
+
+      expect($startButton.length).toBe(1);
+    });
+  });
+});
